ad59eca8d25f3a046e234db0d7b2d68a3b740da4
Альбина Фролова <afrolova@na.edu>
2016-11-22 14:02:48 -0600

    Added python Swap temporary variable project

---
+++ SwapVariables.py
@@ -0,0 +1,14 @@
+#Python program to swap tqo variables
+
+#To take input from the user
+
+x = 5
+y = 10
+
+#create a temporary variable and swap the values 
+temp = x
+x = y 
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
