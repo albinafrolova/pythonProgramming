059247cec107189aa527f78bc77162c2936e1717
Albina Frolova <afrolova@na.edu>
2017-01-25 20:02:29 -0600

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -14,7 +14,6 @@ def divide(a, b):
     print "DIVIDING %d / %d" % (a, b)
     return a / b
 
-
 print "Let's do some math with just functions!"
 
 age = add(30, 5)
@@ -23,9 +22,6 @@ weight = multiply(90, 2)
 iq = divide(100, 2)
 
 print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
-
-
-# A puzzle for the extra credit, type it in anyway.
 print "Here is a puzzle."
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
