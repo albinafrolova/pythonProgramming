b7ccdae07818f7461c09245709bff2d5cab99a65
Альбина Фролова <afrolova@na.edu>
2017-06-11 17:53:11 +0600

    Added new version of profanity checker

---
+++ ProfanityChecker/UpdatedVersion.py
@@ -0,0 +1,17 @@
+import urllib
+
+def read_text():
+    quotes = open("/Users/albinafrolova/Desktop/MovieQuotes.txt")
+    contents_of_file = quotes.read()
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!!")
+    elif "false" in output:
+        print("This document has no curse words!")
+read_text()
